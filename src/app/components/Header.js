cd3eb31058ed8c48b5eed6c0cfee204026e5e9c3
Kait Rice <kaitnrice@gmail.com>
2024-12-30 10:56:07 -0800

    Add Hero

---
+++ src/app/components/Header.js
@@ -0,0 +1,13 @@
+// import './Header.css';
+
+
+
+function Header() {
+  return (
+    <div id='header'>
+
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/app/components/Hero.js
@@ -0,0 +1,20 @@
+import './Hero.css';
+
+function Hero() {
+  return (
+    <header id="hero">
+      <h1>Kait Rice</h1>
+      <h2>Full-Stack Developer</h2>
+      {/* <a
+        className="link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn more
+      </a> */}
+    </header>
+  );
+}
+
+export default Hero;
